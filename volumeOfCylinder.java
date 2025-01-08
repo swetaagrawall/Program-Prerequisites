3877535fb5b85f1ff8e1887a47c369588d7023b2
sweta <swetaarawal972@gmail.com>
2025-01-08 10:18:44 +0530

    volumeOfCylinder.java is created

---
+++ volumeOfCylinder.java
@@ -0,0 +1,11 @@
+public class volumeOfCylinder{  
+    public static void main(String args[])  
+    {  
+     Scanner sc = new Scanner(System.in);
+    double height=sc,nextInt();  
+    double radius=sc.nextInt();  
+    double pie=3.14285714286;  
+    double volume=pie*(radius*radius)*height;  
+    System.out.println("Volume of the cylinder="+volume);  
+     }  
+}  
