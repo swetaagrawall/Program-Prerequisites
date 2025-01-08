8deeb72af0f5fa5ad87522821cc39d18d37af229
sweta <swetaarawal972@gmail.com>
2025-01-08 13:02:41 +0530

    deleted

--- Sum.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Sum{
-  public static void main(String[] args){
-   Scanner sc= new Scanner(System.in);
-   int a=sc.nextInt();
-   int b=sc.nextInt();
-     System.out.println("Sum is : "+ a+b);
-}
-}
--- TemperatureConversion.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class TemperatureConversion{
-  public static void main(String[] args){
-   Scanner sc= new Scanner(System.in);
-    System.out.println("Enter temperature in celsius :");
-    double a=sc.nextDouble();
-     System.out.println("Temperature in Fahrengeit is : "+ (a*(9/5)+32));
-}
-}
