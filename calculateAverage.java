6ef28198f5ab8611c8605f1221425b6f5104b547
sweta <swetaarawal972@gmail.com>
2025-01-08 10:22:40 +0530

    calculateAvg.java is created

---
+++ calculateAverage.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class calculateAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter the second number:");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Enter the third number:");
+        double num3 = sc.nextDouble();
+
+        double average = (num1 + num2 + num3) / 3;
+
+        System.out.println("The average of the three numbers is:" + average);
+    }
+}
