337107e069141317ca8cbf0be014e4747af5bbfa
sweta <swetaarawal972@gmail.com>
2025-01-08 10:21:07 +0530

    calculateSimpleInterest.java is created

---
+++ calculateSimpleInterest.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class calculateSimpleInterest{
+    public static void main(String args[])
+    {   
+	float Principal, Rate, Time;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter Principal");
+	Principal = sc.nextInt();
+        System.out.println("Enter Rate");
+	Rate = sc.nextInt();
+	System.out.println("Enter Time");
+	Time = sc.nextInt();
+        
+        float Simple_Interest = (Principal * Time * Rate) / 100;
+        System.out.println("Simple interest = " + Simple_Interest);
+    }
+}
