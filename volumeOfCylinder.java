2b76eedd0407b73c7d4a32c9610bd413400929b2
sweta <swetaarawal972@gmail.com>
2025-01-08 11:50:13 +0530

    Volume of cylinder is updated

--- volumeOfCylinder.java
+++ volumeOfCylinder.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class VolumeOfCylinder{  
     public static void main(String args[])  
     {  
      Scanner sc = new Scanner(System.in);
-    double height=sc,nextInt();  
-    double radius=sc.nextInt();  
+    double height=sc.nextDouble();
+      double radius=sc.nextDouble();  
     double pie=3.14285714286;  
     double volume=pie*(radius*radius)*height;  
     System.out.println("Volume of the cylinder="+volume);  
